1cde0a0249506fce4aa2063735c6a0b8838eb8b8
baasankhuu-p <purevbaasankhuu@gmail.com>
2024-06-16 19:38:42 +0800

    update

--- src/components/render/schedule.tsx
+++ src/components/render/schedule.tsx
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
 import React from "react";
-import { MerchantServiceSchedule } from "service/merchantService/type";
+// import { MerchantServiceSchedule } from "service/merchantService/type";
 import { capitalizate } from "utils/index";
 
 type Props = {
-  hours?: MerchantServiceSchedule[];
+  hours?: any[];
 };
 export const RenderServiceSchedule = ({ hours }: Props) => {
   return (
@@ -12,9 +12,8 @@ export const RenderServiceSchedule = ({ hours }: Props) => {
       {hours?.map((el, index) => (
         <div
           className="flex items-center gap-1 text-gray-600"
-          key={"time-table-" + index}
-        >
-          <div>{el?.week_days?.map((el) => capitalizate(el))?.join("-")}</div>
+          key={"time-table-" + index}>
+          {/* <div>{el?.week_days?.map((el) => capitalizate(el))?.join("-")}</div> */}
           <div>
             {dayjs(el?.opening).format("HH:mm")}-
             {dayjs(el?.closing).format("HH:mm")}
