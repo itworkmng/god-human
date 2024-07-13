8b56b1afbc3eb685b12e95697ec770d3d0b1830e
baasankhuu-p <purevbaasankhuu@gmail.com>
2024-07-13 14:30:38 +0800

    updated

--- src/services/company/index.tsx
+++ src/services/company/index.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { ICompany } from "./types";
-
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://info.ebarimt.mn/rest/merchant"
+    : "/api";
 namespace company {
   export const info = async (register: string): Promise<ICompany> => {
     try {
       const response = await axios.get<ICompany>(
-        `https://info.ebarimt.mn/rest/merchant/info`, // Proxy path
+        `${BASE_URL}/info`, // Proxy path
         {
           params: { regno: register },
           headers: {
